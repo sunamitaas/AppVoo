184c83de078b4f2b204aa618cdae8a2932f044df
Sunamita Santos <123337196+sunamitaas@users.noreply.github.com>
2024-03-20 18:47:44 -0400

    Add files via upload
    Este é um aplicativo que realiza faça a reserva do voo, ele Verifica próxima poltrona livre, Verifica se uma poltrona está ocupada, Ocupar uma poltrona selecionada e Verifica quantidade de vagas disponiveis no voo.

---
+++ AppVoo2/AppVoo2/Program.cs
@@ -0,0 +1,103 @@
+﻿// Sunamita 
+// AppVoo
+
+/*
+
+TESTES:
+
+Voo voo = new Voo(10, 001, new DateTime());
+voo.poltronas[0] = true;
+voo.poltronas[1] = true;
+
+Console.WriteLine(voo.ProximoLivre());
+
+Console.WriteLine("Digite o numero da poltrona que você quer: ");
+int pol = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(voo.Verifica(pol));
+voo.Ocupa(pol);
+Console.WriteLine(voo.Verifica(pol));
+
+Console.WriteLine("Numero de vagas disponiveis: ");
+Console.WriteLine(voo.Vagas());
+
+for (int i = 0; i < voo.qtdVagas; i++)
+{
+
+    Console.WriteLine(voo.poltronas[i]);
+
+}
+*/
+
+// MENU - RESERVAR PASSAGEM
+
+//Criar uma instância da classe Voo
+
+
+Voo voo = new Voo(10, 123, new DateTime(2024, 3, 20));
+
+bool pesquisar = true;
+
+while (pesquisar)
+{
+    Console.WriteLine("Ola, faça a reserva da sua passagem por aqui");
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1 - Verificar próxima poltrona livre");
+    Console.WriteLine("2 - Verificar se uma poltrona está ocupada");
+    Console.WriteLine("3 - Ocupar uma poltrona");
+    Console.WriteLine("4 - Verificar quantidade de vagas");
+    Console.WriteLine("5 - Sair");
+
+    int opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            int proximaPoltrona = voo.ProximoLivre();
+
+            if (proximaPoltrona != -1)                   // -1 para encaixar no vetor
+                Console.WriteLine($"{proximaPoltrona}");
+            else
+                Console.WriteLine("Não há poltronas livres.");
+        break;
+
+        case 2:
+
+            Console.WriteLine("Digite o número da poltrona:");
+            int numeroPoltrona = int.Parse(Console.ReadLine());
+
+            if (voo.Verifica(numeroPoltrona))
+                Console.WriteLine("A poltrona não está livre.");
+            else
+                Console.WriteLine("A poltrona está livre.");
+        break;
+
+        case 3:
+            Console.WriteLine("Digite o número da poltrona:");
+            int numPoltrona = int.Parse(Console.ReadLine());
+
+            if (voo.Ocupa(numPoltrona))
+                Console.WriteLine("Poltrona ocupada com sucesso.");
+            else
+                Console.WriteLine("A Poltrona não foi ocupada");
+        break;
+
+        case 4:
+            int vagasLivres = voo.Vagas();
+            Console.WriteLine($"Número de vagas livres: {vagasLivres}");
+            break;
+
+        case 5:
+            pesquisar = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+
+    Console.WriteLine(); // Linha em branco
+}
+
+
---
+++ AppVoo2/AppVoo2/Voo.cs
@@ -0,0 +1,129 @@
+﻿
+
+// tire a interação do usuario da sua logica
+// aqui é a logica
+
+public class Voo
+{
+    //public DateOnly dataVoo;
+    //public TimeOnly timeVoo;
+
+    private DateTime dataVoo;
+    private int numVoo;
+    public int qtdVagas;
+    public bool[] poltronas;          // usar vetor ou lista 
+
+
+    //CONSTRUTOR
+    // O que é o metodo Construtor? toda vez que eu inicio uma instancia ele ja executa.
+    //this ele referencia a variavel fora do metodo, isso é feito para diferenciar a variavel de entrada do metodo da variavel da classe la emcima. 
+
+    public int getnumVoo()
+    {
+        return numVoo;
+    }
+
+    public DateTime getdatavoo()
+    {
+        return dataVoo;
+    }
+
+    public Voo (int qtdVagas, int numVoo, DateTime dataVoo)
+    {
+
+        this.dataVoo = dataVoo;
+        this.numVoo = numVoo;
+        poltronas = new bool[qtdVagas];
+        this.qtdVagas = qtdVagas;
+
+    }
+
+
+    // Poltrona Livre
+
+    public int ProximoLivre()
+    {
+        for (int i = 0; i < poltronas.Length; i++)   
+        {
+            if (poltronas[i] == false)
+            {
+                int poltronaLivre = i + 1;
+                Console.WriteLine("A Proxima poltrona livre é: ");
+                return poltronaLivre;                                                    // +1 é para add o indice
+            }
+
+        }
+
+        return -1 ;
+    }
+
+    // verifica se a poltrona esta ocupada
+
+    public bool Verifica(int numPoltrona)
+    {
+
+        if (poltronas[numPoltrona - 1] == true)
+        {
+            Console.WriteLine("A Poltrona não esta livre");
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    // Método Ocupa
+    /*
+    public bool Ocupa (int numPoltrona)
+    {
+        if (this.Verifica(numPoltrona) == true)
+        {
+            poltronas[numPoltrona - 1] = true;
+            Console.WriteLine("A Poltrona esta ocupada");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("A Poltrona não esta ocupada");
+            return false;
+        }
+    }
+    */
+
+    public bool Ocupa(int numPoltrona)
+    {
+        if (this.Verifica(numPoltrona))
+        {
+            // Console.WriteLine("A Poltrona não esta Livre");
+            return false;
+            
+        }
+        else
+        {
+            poltronas[numPoltrona - 1] = true;
+            // Console.WriteLine("A Poltrona foi ocupada com Sucesso");
+            return true;
+        }
+    }
+
+    //vagas 
+
+    public int Vagas ()
+    {
+        int vagasLivres = 0;
+        for(int i = 0;i < poltronas.Length; i++)
+        {
+
+            if (poltronas[i] == false)
+            {
+                vagasLivres += 1;
+            }
+            
+        }
+        return vagasLivres;
+    }
+
+}
